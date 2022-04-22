118d636970008d99c88209e5fd0157e97101afc0
Mario Lazzari <mario.lazzari@italcoeng.com>
2022-04-22 10:57:16 +0200

    Done lesson 40

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,12 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState([]);
+
+  const onSearchChange = e => {
+    const searchTerm = e.target.value;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setFilteredMonsters(filteredMonsters);
+  };
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(data => {
+        setMonsters(data);
+        setFilteredMonsters(data);
+      });
+  }, []);
 
   return (
     <div className="App">
-      {monsters.map(monster => (
+      <input
+        className="search-text"
+        type="search"
+        placeholder="Find monster..."
+        onChange={onSearchChange}
+      />
+
+      {filteredMonsters.map(monster => (
         <div key={monster.id}>
           <h1>{monster.name}</h1>
         </div>
