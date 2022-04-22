75091a8fb8a26463ba36094045213fca88e49dc3
Mario Lazzari <mario.lazzari@gmail.com>
2022-04-22 19:47:26 +0200

    Done lesson 74

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        <div className="category-body-container">
+          <h1>Hats</h1>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
