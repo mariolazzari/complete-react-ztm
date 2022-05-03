0d5fadb245b9cad38ded8e4d7a07d7a9ef321ad4
Mario Lazzari <mario.lazzari@gmail.com>
2022-05-03 20:42:10 +0200

    Done lesson 103

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,18 +1,20 @@
 import { Routes, Route } from "react-router-dom";
-import MenuBar from "./views/MenuBar";
-import Home from "./views/Home";
-import Shop from "./views/Shop";
-import SignIn from "./views/SignIn";
-import SignUp from "./views/SignUp";
+
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<MenuBar />}>
+      <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
-        <Route path="sign-up" element={<SignUp />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
