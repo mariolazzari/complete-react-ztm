2a0f781325818b041c9b594e8dbfa7004591fe0e
Mario Lazzari <mario.lazzari@gmail.com>
2022-09-04 11:26:20 +0200

    Done lesson 184

--- crwn-clothing-v2/src/routes/categories-preview/categories-preview.component.jsx
+++ crwn-clothing-v2/src/routes/categories-preview/categories-preview.component.jsx
@@ -1,13 +1,13 @@
-import { Fragment } from "react";
-import { useSelector } from "react-redux";
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
 
 import {
   selectCategoriesMap,
   selectCategoriesIsLoading,
-} from "../../store/categories/category.selector";
+} from '../../store/categories/category.selector';
 
-import CategoryPreview from "../../components/category-preview/category-preview.component";
-import Spinner from "../../components/spinner/spinner.component";
+import CategoryPreview from '../../components/category-preview/category-preview.component';
+import Spinner from '../../components/spinner/spinner.component';
 
 const CategoriesPreview = () => {
   const categoriesMap = useSelector(selectCategoriesMap);
@@ -18,7 +18,7 @@ const CategoriesPreview = () => {
       {isLoading ? (
         <Spinner />
       ) : (
-        Object.keys(categoriesMap).map(title => {
+        Object.keys(categoriesMap).map((title) => {
           const products = categoriesMap[title];
           return (
             <CategoryPreview key={title} title={title} products={products} />
