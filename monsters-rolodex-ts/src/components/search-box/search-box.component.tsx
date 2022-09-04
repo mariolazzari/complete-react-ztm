110347da051b12f0a96ff17f6482a1d79c213805
Mario Lazzari <mario.lazzari@gmail.com>
2022-09-04 18:50:59 +0200

    Done lesson 194

---
+++ monsters-rolodex-ts/src/components/search-box/search-box.component.tsx
@@ -0,0 +1,23 @@
+import { ChangeEventHandler } from "react";
+import "./search-box.styles.css";
+
+type ISearchBoxProps = {
+  className: string;
+  placeholder?: string;
+  onChangeHandler: ChangeEventHandler<HTMLInputElement>;
+};
+
+const SearchBox = ({
+  className,
+  placeholder,
+  onChangeHandler,
+}: ISearchBoxProps) => (
+  <input
+    className={`search-box ${className}`}
+    type="search"
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
+
+export default SearchBox;
