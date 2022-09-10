4b7464bd895a92256648fa3ce82f8742fa436319
Mario Lazzari <mario.lazzari@gmail.com>
2022-09-10 16:42:15 +0200

    Done lesson 235

--- crwn-clothing-v2/netlify/functions/create-payment-intent.js
+++ crwn-clothing-v2/netlify/functions/create-payment-intent.js
@@ -1,14 +1,14 @@
-require("dotenv").config();
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-exports.handler = async event => {
+exports.handler = async (event) => {
   try {
     const { amount } = JSON.parse(event.body);
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
-      currency: "usd",
-      payment_method_types: ["card"],
+      currency: 'usd',
+      payment_method_types: ['card'],
     });
 
     return {
@@ -19,7 +19,7 @@ exports.handler = async event => {
     console.log({ error });
 
     return {
-      statusCode: 400,
+      status: 400,
       body: JSON.stringify({ error }),
     };
   }
