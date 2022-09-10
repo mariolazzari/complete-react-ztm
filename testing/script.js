80f7dfc2e3bd7dc84a397a13093cda21cff2d38c
Mario Lazzari <mario.lazzari@gmail.com>
2022-09-10 21:59:36 +0200

    DOne lesson 272

---
+++ testing/script.js
@@ -0,0 +1,16 @@
+const sites = [
+  "site1.com",
+  "mario.it",
+  "javascript.net",
+  "google.com",
+  "nodejs.org",
+  "reactjs.org£",
+];
+
+const search = site => {
+  const res = sites.filter(s => s.includes(site));
+
+  return res.length > 3 ? res.slice(0, 3) : res;
+};
+
+console.log(search(""));
